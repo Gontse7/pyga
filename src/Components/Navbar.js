1a7eda50a9ba7df7f0e1dd8a1e6c1f48abd43142
Gontse7 <gontsemogana7@gmail.com>
2025-03-17 16:26:09 +0200

    fixed the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,34 @@
-
-import { MenuItems } from "./menuItems"
-import "./Navbar.css"
+import React, { useState } from "react";
+import { MenuItems } from "./menuItems";
+import "./Navbar.css";
 
 const Navbar = () => {
-    state = {clicked:false};
-    handleClick = () => {this.setState({clicked: !this.state.clicked})};
-    return(
-        <nav className="navbarItems">
-            <h1 className="nav-logo">PYGA</h1>
-            <div className="menu-icons" onClick={this.handleClick}>
-                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                
-            </div>
-            <ul className="nav-menu">
-                {MenuItems.map((item, index) => {
-                    return (
-                        <li key = {index}>
-                            <a href="{item.url}" className="{item.cName}">
-                            {item.tittle}</a>
-                        </li>
-                    )
-                })}
-            </ul>
-            <button>Contact</button>
-        </nav>
-    )
-}
-export default Navbar
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <nav className="navbarItems">
+      <h1 className="nav-logo">PYGA</h1>
+      <div className="menu-icons" onClick={handleClick}>
+        <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className="nav-menu">
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={item.url} className={item.cName}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <button>Contact</button>
+    </nav>
+  );
+};
+
+export default Navbar;
